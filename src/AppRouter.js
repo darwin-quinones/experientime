beee9e7fe0f704c83fd3aa40dfa8788c46cc09f9
darwin-quinones <darwinquinones735@gmail.com>
2023-08-14 11:06:20 -0500

    I advanced the home page with some animations and info

--- src/AppRouter.js
+++ src/AppRouter.js
@@ -1,16 +1,13 @@
 import React from 'react';
 import {Route, Routes } from 'react-router-dom';
-import SitebarMenu from './components/SidebarMenu';
-import CarCreate from './components/projects/cars/CarCreate';
 import CarList from './components/projects/cars/CarList';
-import ReportPDF from './components/projects/reports/ReportPDF';
-import Dashboard from './components/Dashboard';
 import PhotoList from './components/projects/photos/PhotoList';
+import HomePage from './HomePage';
 
 function AppRouter() {
     return (
         <Routes>
-            <Route path='/' element={<Dashboard />} />
+            <Route path='/' element={<HomePage />} />
             <Route path='/cars' element={<CarList />} />
             <Route path='/photos' element={<PhotoList />} />
         </Routes>
--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,11 +1,58 @@
-import React from 'react';
+import React, { useEffect, useRef } from 'react';
+import TypeIt from 'typeit';
 
 const HomePage = () => {
+    const typewriterRef = useRef(null);
+
+    useEffect(() => {
+        const typewriter = new TypeIt(typewriterRef.current, {
+            strings: ['Welcome to Experientime!'],
+            speed: 100,
+            startDelay: 500,
+            lifeLike: true,
+            loop: true,
+        })
+            .pause(1000)
+            .delete(7)
+            .pause(500)
+            .go();
+
+        return () => {
+            typewriter.destroy();
+        };
+    }, []);
+
+
     return (
-        <div>
-            Home page
+        <div className="container">
+            <div className="py-5">
+                <h1 ref={typewriterRef}></h1>
+                <p className="lead">
+                    Hello! I'm Darwin, a Full Stack developer. Experientime is a project
+                    designed to showcase my expertise and experiences using APIs,
+                    React.js, Laravel, and MySQL.
+                </p>
+                <p>
+                    Join me on this journey as we explore a captivating gallery created
+                    using Unsplash's API. Discover and download a diverse range of photos
+                    as you scroll through the collection.
+                </p>
+                <p>
+                    The car section introduces my Laravel-based API, powering a complete
+                    CRUD system for managing vehicles. Experience seamless interactions as
+                    you create, read, update, and delete cars. Additionally, you can
+                    download car information in PDF and Excel formats.
+                </p>
+                <p>
+                    Simplifying the process of adding new cars, I've integrated a public
+                    API of countries. This ensures effortless selection of the origin
+                    country. As I continue to innovate, watch for exciting new features
+                    and enhancements.
+                </p>
+                <p className="lead">Thank you for your interest in Experientime!</p>
+            </div>
         </div>
     );
-}
+};
 
 export default HomePage;
