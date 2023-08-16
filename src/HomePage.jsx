a3a2a0a6e7207f6ca1fca384695bcab031838620
Darwin Quiñones <darwinquinones735@gmail.com>
2023-08-15 22:12:23 -0500

    I added home information about experientime

--- src/HomePage.jsx
+++ src/HomePage.jsx
@@ -1,58 +1,60 @@
-import React, { useEffect, useRef } from 'react';
-import TypeIt from 'typeit';
+import React, { useEffect, useRef } from "react";
+import TypeIt from "typeit";
 
 const HomePage = () => {
-    const typewriterRef = useRef(null);
+  const typewriterRef = useRef(null);
 
-    useEffect(() => {
-        const typewriter = new TypeIt(typewriterRef.current, {
-            strings: ['Welcome to Experientime!'],
-            speed: 100,
-            startDelay: 500,
-            lifeLike: true,
-            loop: true,
-        })
-            .pause(1000)
-            .delete(7)
-            .pause(500)
-            .go();
+  useEffect(() => {
+    // adding a typing animation that goes back and forth infinitely
+    const typewriter = new TypeIt(typewriterRef.current, {
+      strings: ["Welcome to Experientime!"],
+      speed: 100,
+      startDelay: 500,
+      lifeLike: true,
+      loop: true,
+    })
+      .pause(1000)
+      .delete(7)
+      .pause(500)
+      .go();
 
-        return () => {
-            typewriter.destroy();
-        };
-    }, []);
+    return () => {
+      typewriter.destroy();
+    };
+  }, []);
 
-
-    return (
-        <div className="container">
-            <div className="py-5">
-                <h1 ref={typewriterRef}></h1>
-                <p className="lead">
-                    Hello! I'm Darwin, a Full Stack developer. Experientime is a project
-                    designed to showcase my expertise and experiences using APIs,
-                    React.js, Laravel, and MySQL.
-                </p>
-                <p>
-                    Join me on this journey as we explore a captivating gallery created
-                    using Unsplash's API. Discover and download a diverse range of photos
-                    as you scroll through the collection.
-                </p>
-                <p>
-                    The car section introduces my Laravel-based API, powering a complete
-                    CRUD system for managing vehicles. Experience seamless interactions as
-                    you create, read, update, and delete cars. Additionally, you can
-                    download car information in PDF and Excel formats.
-                </p>
-                <p>
-                    Simplifying the process of adding new cars, I've integrated a public
-                    API of countries. This ensures effortless selection of the origin
-                    country. As I continue to innovate, watch for exciting new features
-                    and enhancements.
-                </p>
-                <p className="lead">Thank you for your interest in Experientime!</p>
-            </div>
-        </div>
-    );
+  return (
+    <div className="container">
+      <div className="py-5">
+        <h1 ref={typewriterRef}></h1>
+        <p className="lead">
+          Hello! My name is Darwin, and I'm a Full Stack developer. Experientime
+          is a project I've developed with the objective of showcasing my expertise 
+          and experiences using APIs, React.js, Laravel, and MySQL.
+        </p>
+        <p>
+          In this engaging project, I leverage Unsplash's API to create a
+          captivating gallery where users can view, filter, and download a
+          variety of photos. As you scroll down, you'll discover a diverse
+          collection of pictures to enjoy.
+        </p>
+        <p>
+          In the car section, I utilize my own Laravel-based API, which serves
+          data to the front-end. I've implemented a comprehensive CRUD
+          functionality, empowering users to effortlessly create, read, update,
+          and delete cars. Moreover, there are two additional options to
+          download car information in both PDF and Excel formats.
+        </p>
+        <p>
+          When adding a new car, I've integrated a public API of countries to
+          simplify the process of selecting the origin country. My ongoing
+          commitment is to continually enhance and introduce new functionalities
+          to this project.
+        </p>
+        <p className="lead">Thank you very much for your interest!</p>
+      </div>
+    </div>
+  );
 };
 
 export default HomePage;
